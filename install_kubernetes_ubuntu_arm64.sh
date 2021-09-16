fe6cdd7953fb439aaea1e687da372bd91977e439
Hong Jinwoo <jinwoo17962@icloud.com>
2021-09-16 13:08:24 +0900

    added : installer

---
+++ install_kubernetes_ubuntu_arm64.sh
@@ -0,0 +1,56 @@
+#bin/bash
+
+echo "install docker first"
+sudo apt-get update
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io 
+
+echo \
+`
+{
+"exec-opts": ["native.cgroupdriver=systemd"]
+}
+` | sudo tee /etc/docker/daemon.json > /dev/null
+
+
+
+echo "Letting iptables see bridged traffic"
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sudo sysctl --system
+
+echo "!!!!! Disabling firewall !!!!!!!"
+sudo ufw disable
+
+echo "update apt and installl dependency"
+sudo apt-get update
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+echo "Installed kubelet, kubeadm, kubectl"
