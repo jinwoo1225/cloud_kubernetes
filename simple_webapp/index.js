5679091880b0fdb1d8a7b8d0fc4e76f7a0086d28
jinwoo1225 <jinwoo17962@icloud.com>
2021-10-01 21:09:32 +0900

    added : process.env support and logger

--- simple_webapp/index.js
+++ simple_webapp/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 3000;
-app.use(express.json());    
+const port = process.env.PORT || 3000;
+app.use(express.json());
 
 app.get("/", (req, res, next) => {
+  const {ip, method, originalUrl} = req;
+  console.log(`${method} ${originalUrl} ${ip}`)
   res.json({
     state: true,
     message: "Hello world!",
